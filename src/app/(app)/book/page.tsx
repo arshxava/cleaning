bef7f2f3dc4976ac86de8e5aad1064ea1ec483c6
Arshdeep Kaur <kaurarshdeep1100@gmail.com>
2025-09-10 12:09:21 +0000

    i want the time zones to be 11-1 1-3 3-5 5-7

--- src/app/(app)/book/page.tsx
+++ src/app/(app)/book/page.tsx
@@ -50,12 +50,10 @@ const steps = [
 ];
 
 const timeSlots = [
-  '09:00 AM',
-  '10:00 AM',
-  '11:00 AM',
-  '01:00 PM',
-  '02:00 PM',
-  '03:00 PM',
+  '11:00 AM - 01:00 PM',
+  '01:00 PM - 03:00 PM',
+  '03:00 PM - 05:00 PM',
+  '05:00 PM - 07:00 PM',
 ];
 
 const timezones = [
@@ -259,7 +257,7 @@ export default function BookingPage() {
                      </Select>
                   </div>
                 </div>
-              <div className="grid grid-cols-2 gap-2 self-start">
+              <div className="grid grid-cols-1 gap-2 self-start">
                 {timeSlots.map((slot) => (
                   <Button
                     key={slot}
