e5de883a297845124f5849748971e10f92ec3191
Arshdeep Kaur <kaurarshdeep1100@gmail.com>
2025-09-24 11:43:42 +0000

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            }
-        ]
-    }
+// next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
